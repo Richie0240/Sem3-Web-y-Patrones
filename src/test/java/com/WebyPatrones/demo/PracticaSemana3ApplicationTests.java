e9206df7d1799df008c5191f3ecb5add4a6da276
ricar <ricar@DESKTOP-ILTFSCI>
2023-10-02 14:19:12 -0600

    SEMANA 3 practica web

---
+++ src/test/java/com/WebyPatrones/demo/PracticaSemana3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WebyPatrones.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaSemana3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
